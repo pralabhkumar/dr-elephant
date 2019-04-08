2cda035fc8ad14d5ab8940654989857287e46a04
Pralabh Kumar <pkumar2@linkedin.com>
2019-04-08 13:05:00 +0530

    Intial HBT for Pig

---
+++ app/com/linkedin/drelephant/tuning/hbt/MRJob.java
@@ -0,0 +1,81 @@
+package com.linkedin.drelephant.tuning.hbt;
+
+import com.linkedin.drelephant.tuning.engine.MRExecutionEngine;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import models.AppHeuristicResult;
+import models.AppHeuristicResultDetails;
+import models.AppResult;
+import org.apache.log4j.Logger;
+
+
+/**
+ * This class represents Map Reduce Job and the suggested parameter and the Map Reduce Job level.
+ */
+public class MRJob {
+  private final Logger logger = Logger.getLogger(getClass());
+  private Map<String, Double> suggestedParameter = null;
+  private List<MRApplicationData> applications = null;
+  private Map<String, String> appliedParameter = null;
+  private List<AppResult> applicationResults = null;
+
+  public MRJob(List<AppResult> results) {
+    this.applicationResults = results;
+    this.appliedParameter = new HashMap<String, String>();
+    setAppliedParameter(results);
+    applications = new ArrayList<MRApplicationData>();
+    suggestedParameter = new HashMap<String, Double>();
+  }
+
+  private void setAppliedParameter(List<AppResult> results) {
+    if (results != null && results.size() >= 0) {
+      AppResult appResult = results.get(0);
+      for (AppHeuristicResult appHeuristicResult : appResult.yarnAppHeuristicResults) {
+        if (appHeuristicResult != null && appHeuristicResult.heuristicName.equals("MapReduceConfiguration")) {
+          for (AppHeuristicResultDetails appHeuristicResultDetails : appHeuristicResult.yarnAppHeuristicResultDetails) {
+            appliedParameter.put(appHeuristicResultDetails.name, appHeuristicResultDetails.value);
+          }
+        }
+      }
+    }
+  }
+
+  public Map<String, String> getAppliedParameter() {
+    return this.appliedParameter;
+  }
+
+  public List<MRApplicationData> getApplicationAnalyzedData() {
+    return applications;
+  }
+
+  public void analyzeAllApplications() {
+    for (AppResult result : this.applicationResults) {
+      MRApplicationData mrApplicationData = new MRApplicationData(result, appliedParameter);
+      applications.add(mrApplicationData);
+    }
+  }
+
+  public void processJobForParameter() {
+    for (MRApplicationData mrApplicationData : applications) {
+      Map<String, Double> applicationSuggestedParameter = mrApplicationData.getSuggestedParameter();
+      for (String parameterName : applicationSuggestedParameter.keySet()) {
+        Double valueSoFar = suggestedParameter.get(parameterName);
+        if (valueSoFar == null) {
+          suggestedParameter.put(parameterName, applicationSuggestedParameter.get(parameterName));
+        } else {
+          Double valueAsperCurrentApplication = applicationSuggestedParameter.get(parameterName);
+          suggestedParameter.put(parameterName, Math.max(valueSoFar, valueAsperCurrentApplication));
+        }
+      }
+    }
+  }
+
+  public Map<String, Double> getJobSuggestedParameter() {
+    return this.suggestedParameter;
+  }
+
+}
