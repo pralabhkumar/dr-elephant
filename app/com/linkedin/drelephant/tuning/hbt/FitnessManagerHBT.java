9e55fb0ceaa7ca22944968a921af8b94b4941834
Pralabh Kumar <pkumar2@linkedin.com>
2019-04-29 15:37:10 +0530

    Pig HBT changes

--- app/com/linkedin/drelephant/tuning/hbt/FitnessManagerHBT.java
+++ app/com/linkedin/drelephant/tuning/hbt/FitnessManagerHBT.java
@@ -103,9 +103,7 @@ public class FitnessManagerHBT extends AbstractFitnessManager {
     if (tuningJobDefinition.averageResourceUsage == null && totalExecutionTime != 0) {
       updateTuningJobDefinition(tuningJobDefinition, jobExecution);
     }
-
     try {
-
       if (isRetried) {
         logger.info(" Retried execution " + jobExecution.id + " for parameter " + jobSuggestedParamSet.id);
         handleRetryScenarios(jobSuggestedParamSet, jobExecution);
@@ -113,7 +111,6 @@ public class FitnessManagerHBT extends AbstractFitnessManager {
         logger.info(" Non Retried execution " + jobExecution.id + " for parameter " + jobSuggestedParamSet.id);
         handleNonRetryScenarios(jobSuggestedParamSet, jobExecution);
       }
-
       logger.info(
           " Calculating Fitness for " + jobSuggestedParamSet.id + " " + jobSuggestedParamSet.fitnessJobExecution.id
               + " " + jobExecution.id);
